4ef950438bc4e5928825dbc9ed2db758ea144fc1
Daniel-code666 <dmartinezcifuentes180@gmail.com>
2021-09-07 22:33:47 -0500

    Angular material tabla y barra de progreso

--- AngularMaterial/src/app/app.component.ts
+++ AngularMaterial/src/app/app.component.ts
@@ -1,10 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 11, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 12, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 13, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 14, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 15, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 16, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 17, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 18, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 19, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 20, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 21, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 22, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
+
+  constructor() {
+    this.progresValue = 0;
+    this.rangeArray = new Array(100);
+  }
+
+  progresValue: number;
+  rangeArray: number[];
+
   title = 'AngularMaterial';
+
+  displayedColumns: string[] = ['name', 'weight', 'symbol', 'position'];
+  columnsToDisplay: string[] = this.displayedColumns.slice();
+  data: PeriodicElement[] = ELEMENT_DATA;
+  isLoading = false;
+
+  ngOnInit(): void {
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(){
+    const element = document.documentElement,
+    body = document.body,
+    scrollTop = 'scrollTop',
+    scrollHeight = 'scrollHeight';
+    this.progresValue = (element[scrollTop] || body[scrollTop]) / ((element[scrollHeight] || body[scrollHeight]) - element.clientHeight) * 100;
+  }
+
+  addColumn() {
+    const randomColumn = Math.floor(Math.random() * this.displayedColumns.length);
+    this.columnsToDisplay.push(this.displayedColumns[randomColumn]);
+    this.isLoading = true;
+    setTimeout(() => {
+      const randomColumn = Math.floor(Math.random() * this.displayedColumns.length);
+      this.columnsToDisplay.push(this.displayedColumns[randomColumn]);
+      this.isLoading = false;
+      }, 2000);
+  }
+
+  removeColumn() {
+    if (this.columnsToDisplay.length) {
+      this.columnsToDisplay.pop();
+    }
+  }
 }
+
+
+
--- AngularMaterial/src/app/app.module.ts
+++ AngularMaterial/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
