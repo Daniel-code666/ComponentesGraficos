d1e4bf835fbce17b718b3c068f52bf0c392e0051
Daniel-code666 <dmartinezcifuentes180@gmail.com>
2021-09-08 15:33:56 -0500

    Angular bootstrap (tabla)

--- AngularBootstrap/src/app/app.module.ts
+++ AngularBootstrap/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbProgressbarModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
